4a83dc6516210414e79c7820e69a551ba63b003b
linhaorui <2909753663@qq.com>
2022-11-10 10:43:37 +0800

    chore: 基于ts构建项目

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Table from '../views/table/Table.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/table',
+      name: 'table',
+      component: Table
+    },
+  ]
+})
+
+export default router
