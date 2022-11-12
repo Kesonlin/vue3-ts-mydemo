46c660070027877ead9333878cfcb8d19fc14a9e
linhaorui <2909753663@qq.com>
2022-11-12 14:35:28 +0800

    perf：完成中断请求功能

--- src/network/request.ts
+++ src/network/request.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
 
 const baseURL = 'http://127.0.0.1'
 
@@ -6,11 +6,42 @@ const instance = axios.create({
 	baseURL
 })
 
+const map = new Map()
+
 instance.interceptors.request.use((config) => {
 	console.log(config)
+	// const source = axios.CancelToken.source()
+	// config.cancelToken = source.token
+
+	// setTimeout(() => {
+	// 	source.cancel('111')
+	// }, 2000)
 	return config
 })
 
-export function request(config) {
+instance.interceptors.response.use((response: AxiosResponse) => {
+	console.log(response)
+	const cancelToken = response.config.cancelToken
+	if (cancelToken) {
+		map.delete(cancelToken)
+	}
+	console.log(map)
+})
+
+export function request(config, source: any) {
+	if (source) {
+		map.set(config.cancelToken, source)
+	}
 	return instance(config)
+}
+
+export function abortReq() {
+	console.log(map)
+
+	for (const [CancelToken, source] of map.entries()) {
+		console.log(CancelToken)
+		console.log(source)
+
+		source.cancel('取消请求')
+	}
 }
--- src/views/abort/CancelReq.vue
+++ src/views/abort/CancelReq.vue
@@ -2,11 +2,12 @@
 	<button @click="fn(1)">请求1</button>
 	<button @click="fn(2)">请求2</button>
 	<button @click="fn(3)">请求3</button>
-	<button>取消请求</button>
+	<button @click="abort">取消请求</button>
 </template>
 
 <script setup lang="ts">
-import { request } from '../../network/request'
+import { request, abortReq } from '../../network/request'
+import axios from 'axios'
 
 const fn = (i) => {
 	const req = [
@@ -14,14 +15,25 @@ const fn = (i) => {
 		'http://127.0.0.1:9999/abort2',
 		'http://127.0.0.1:9999/abort3'
 	]
-	request({
-		url: req[i - 1],
-		method: 'get'
-		// params: {
-		// 	t: Date.now()
-		// }
-	})
+	const CancelToken = axios.CancelToken
+	const source = CancelToken.source()
+	request(
+		{
+			url: req[i - 1],
+			method: 'get',
+			// 这里记得小写
+			cancelToken: source.token
+			// params: {
+			// 	t: Date.now()
+			// }
+		},
+		source
+	)
+
+	// source.cancel() // 取消请求
 }
+
+const abort = () => abortReq()
 </script>
 
 <style lang="scss"></style>
